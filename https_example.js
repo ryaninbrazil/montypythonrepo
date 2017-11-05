91bbd9363ab7620a5fbdb72adb4eace3cd66f215
Ryan <ryaninbrazil@gmail.com>
2017-11-05 23:02:08 +0000

    third commit

--- https_example.js
+++ https_example.js
@@ -8,12 +8,18 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
+
+// var callback = function() {
+//   console.log('In response handler callback!');
+// }
 
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
